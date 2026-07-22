727f5f824c85401aa1523f23d86817cfd608fc92
jahruz67 <cookies67@gmail.com>
2026-07-21 18:24:22 -0700

    chore(translate): add missing compose runtime imports
    Import `mutableStateOf` and delegate properties `getValue` and `setValue`
    to support state management within `TranslateSettings`.

--- java/src/org/futo/inputmethod/latin/uix/settings/pages/TranslateSettings.kt
+++ java/src/org/futo/inputmethod/latin/uix/settings/pages/TranslateSettings.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
